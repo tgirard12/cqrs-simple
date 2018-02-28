5fcd0169904998dee8b2c1b78a0556ed6b467487
Thomas Girard <thomas.girard12@gmail.com>
2018-02-28 13:59:30 +0100

    Add Event module

---
+++ src/main/kotlin/com/tgirard12/cqrssimple/Event.kt
@@ -0,0 +1,67 @@
+package com.tgirard12.cqrssimple
+
+import org.slf4j.LoggerFactory.getLogger
+
+
+/**
+ *
+ */
+interface Event {
+    val name: String? get() = this::class.simpleName
+}
+
+/**
+ *
+ */
+interface EventBus {
+    fun dispatch(event: Event): Unit
+}
+
+/**
+ *
+ */
+@Suppress("AddVarianceModifier")
+interface EventHandler<E : Event> : Handler {
+    fun handle(event: E): Unit
+}
+
+/**
+ *
+ */
+open class EventHandlerBase<E : Event>(
+        private val handleFun: ((E) -> Unit)? = null
+) : EventHandler<E> {
+
+    override fun handle(event: E): Unit = handleFun?.invoke(event)
+            ?: throw IllegalArgumentException("handle Must be override or set via constructor")
+}
+
+/**
+ *
+ */
+class EventBusImpl(
+        handlerList: List<EventHandler<Event>>
+) : EventBus {
+
+    val log = getLogger("EventBus")
+
+    internal val handlers = handlerList
+            .map {
+                it.javaClass.kotlin.supertypes[0].arguments[0].type.toString() to it
+            }
+            .groupBy({ it.first }, { it.second })
+
+    override fun dispatch(event: Event): Unit {
+        val eventClass = event::class.qualifiedName
+                ?: throw  IllegalArgumentException("Event ${event::class} ::class.qualifiedName NULL")
+
+        handlers[eventClass]
+                ?.let {
+                    it.forEach {
+                        log.debug("${it.name} > ${event.name}")
+                        it.handle(event)
+                    }
+                }
+                ?: log.info("No handler for '${event.name}' Event")
+    }
+}
